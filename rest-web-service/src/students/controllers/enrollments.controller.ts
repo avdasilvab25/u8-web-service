cf8cab7670fc262c25d04fb9793e8eb7433db324
Diego Gutierrez <dagutierrez.17@est.ucab.edu.ve>
2021-02-07 23:40:29 -0400

    Enrollment

--- rest-web-service/src/students/controllers/enrollments.controller.ts
+++ rest-web-service/src/students/controllers/enrollments.controller.ts
@@ -1,4 +1,42 @@
-import { Controller } from '@nestjs/common';
-
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { ApiBody, ApiCreatedResponse, ApiOkResponse, ApiParam, ApiQuery, ApiTags } from '@nestjs/swagger';
+import { Enrollment } from '../entities/enrollment.entity';
+import { EnrollmentsService } from '../services/enrollments.service';
+import { UpdateResult } from 'typeorm';
+@ApiTags('Enrollment')
 @Controller('enrollments')
-export class EnrollmentsController {}
+export class EnrollmentsController {
+  constructor(private readonly enrollmentService: EnrollmentsService) {}
+
+  @ApiBody({
+    description: 'Enrollment to create',
+    type: Enrollment,
+  })
+  @ApiCreatedResponse({
+      description: 'Enrollment created successfully',
+      type: Enrollment,
+  })
+  @Post()
+  public async createEnrollment(@Body() enrollment: Partial<Enrollment>): Promise<Partial<Enrollment>> {
+      return await this.enrollmentService.createEnrollment(enrollment);
+  }
+
+  @ApiParam({
+    description: 'Person id of the enrollment',
+    type: Number,
+    name: 'personId',
+  })
+  @ApiParam({
+    description: 'Section id of the enrollment',
+    type: Number,
+    name: 'sectionId',
+  })
+  @ApiOkResponse({
+      description: 'Enrollment deleted successfully',
+  })
+  @Delete(':id')
+  public async deleteEnrollment(@Param('id') id: number): Promise<UpdateResult> {
+      return await this.enrollmentService.deleteEnrollment(id);
+  }
+
+}
--- rest-web-service/src/students/services/enrollments.service.ts
+++ rest-web-service/src/students/services/enrollments.service.ts
@@ -1,4 +1,61 @@
 import { Injectable } from '@nestjs/common';
-
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository, UpdateResult, SelectQueryBuilder } from 'typeorm';
+import { Enrollment } from '../entities/enrollment.entity';
+import * as moment from 'moment';
+import { Status } from 'src/app/enums/status.enum';
 @Injectable()
-export class EnrollmentsService {}
+export class EnrollmentsService {
+  constructor(
+    @InjectRepository(Enrollment)
+    private readonly enrollmentRepository: Repository<Enrollment>,
+  ) {}
+
+  /**
+     * This method allows to create a enrollment in database.
+     * @param enrollment object to create in database.
+     * @returns { Promise<Enrollment> }
+     */
+    public async createEnrollment(enrollment: Partial<Enrollment>): Promise<Enrollment> {
+      if(enrollment.type != "student" && enrollment.type != "teacher"){
+        throw "Tipo de enlistado no permitido"
+      }
+      if(! await this.getEnrollement(enrollment.person, enrollment.section)){
+        return await this.enrollmentRepository.save(enrollment);
+      }
+      else{
+        throw "Estudiante ya enlistado"
+      }
+    }
+
+    /**
+     * This method allows to retrieve if a person was previously enrolled in a section
+     * @param personId Id of the person
+     * @param sectionId Id of the section
+     * @returns {Promise<Array<Faculty>>}
+     */
+    public async getEnrollement(personId: any, sectionId: any): Promise<Enrollment> {
+      const enrollement= await this.enrollmentRepository
+            .createQueryBuilder('enrollment')
+            .andWhere(`enrollment.person = '${personId}'`)
+            .andWhere(`enrollment.section = '${sectionId}'`)
+            .getOne();
+      return enrollement;
+    }
+
+    /**
+     * This method logically an enrollment
+     * @param enrollmentId Id of the enrollment
+     * @returns {Promise<UpdateResult>}
+     */
+    public async deleteEnrollment(enrollmentId: number): Promise<UpdateResult> {
+
+      return await this.enrollmentRepository.update(enrollmentId, {
+        deletedDate: moment().toISOString(),
+        status: Status.DISABLED,
+      });
+    }
+
+
+
+}
